2ec008735283578e9512a7d581444b65f8f74f6f
unknown <max.xit234@gmail.com>
2025-11-29 20:47:18 +0300

    next task code eating apple mechanic

--- snake.c
+++ snake.c
@@ -8,70 +8,96 @@ struct Snode{
     int y;
 };
 
-void generateCoordsApple(int *xApple, int *yApple);
+void generateCoordsApple(int *xApple, int *yApple, int **field, int *flag);
 void initSnakeNode(struct Snode *sNode, int x, int y);
 void initSnake(struct Snode *prevNode, int length);
 void moveHeadSnake(struct Snode *startNode, int dx, int dy);
 void fillArraySnake(struct Snode *startNode, int **arr);
+void fillFieldByZero(int **field, int height, int width);
 int** initField(int height, int width);
 void freeField(int **arr, int height);
 
 int main() {
     srand(time(NULL));
-    int k = 0;
-    printf("0");
     int **field = initField(25, 25);
-    printf("1");
     int x = 0, y = 0;
-    int xApple = 0, yApple = 0;
+    int xApple = 0, yApple = 0, isAppleOnField = 0;
+    int curDx = 0, curDy = 1, newDx = 10, newDy = 10;
+    char key = '$';
     struct Snode *sHead = malloc(sizeof(struct Snode)); 
     
     printf("Choose coordinats of the beginning from 0 to 24\n");
     scanf("%d %d", &x, &y);
 
-    generateCoordsApple(&xApple, &yApple);
-    printf("3");
     initSnakeNode(sHead, x, y);
-    printf("4");
-    initSnake(sHead, 5);
-    printf("5");
-    int isAppleOnField = 0;
-    
+    initSnake(sHead, 3);
     fillArraySnake(sHead, field);
-    char key = '$';
+
     while (key != 'q') {
 
         key = getchar();
 
-        if (key == 'a') moveHeadSnake(sHead, -1, 0);
-        else if (key == 'w') moveHeadSnake(sHead, 0, -1);
-        else if (key == 's') moveHeadSnake(sHead, 0, 1);
-        else if (key == 'd') moveHeadSnake(sHead, 1, 0);
-        else moveHeadSnake(sHead, 0, 0);
-        
+        if (key == 'a') {
+            newDx = 0;
+            newDy = -1;
+        }
+        else if (key == 'w') {
+            newDx = -1;
+            newDy = 0;
+        }
+        else if (key == 's') {
+            newDx = 1;
+            newDy = 0;
+        }
+        else if (key == 'd') {
+            newDx = 0;
+            newDy = 1;
+        } else {
+            newDx = curDx;
+            newDy = curDy;
+        }
+
+        if ((curDx == -newDx && curDy == 0) || (curDy == -newDy && curDx == 0)) {
+            printf("GAME OVER");
+            break;
+        } else {
+            curDx = newDx;
+            curDy = newDy;
+            moveHeadSnake(sHead, curDx, curDy);
+        }
+
         fillArraySnake(sHead, field);
 
+        if (isAppleOnField == 0) generateCoordsApple(&xApple, &yApple, field, &isAppleOnField);
+        
         for (int i = 0; i < 25; i++) {
             for (int j = 0; j < 25; j++) {
                 if (field[i][j] == 1) {
                     printf("o");
-                } else if (i == xApple && j == yApple) {
+                } else if (i == xApple && j == yApple && isAppleOnField == 1) {
                     printf("*");
+                } else if (i == 0 || j == 0 || i == 24 || j == 24) {
+                    if (i == 0 || i == 24) printf("-");
+                    if ((j == 0 || j == 24) && i != 0 && i != 24) printf("|");
                 } else {
                     printf(" ");
                 }
             }
             printf("\n");
         }
-        printf("\033[H\033[J");
+        //printf("\033[H\033[J");
+        printf("\n");
     }
-    free(field);
+    freeField(field, 25);
     return 0;
 }
 
-void generateCoordsApple(int *xApple, int *yApple) {
-    *xApple = rand() % 25;
-    *yApple = rand() % 25;
+void generateCoordsApple(int *xApple, int *yApple, int **field, int *flag) {
+    do {
+        *xApple = 1 + rand() % 23;
+        *yApple = 1 + rand() % 23;
+        if (*flag == 0) *flag = 1;
+    } while (field[*xApple][*yApple] != 0);
 }
 
 void initSnakeNode(struct Snode *sNode, int x, int y) {
@@ -83,7 +109,7 @@ void initSnakeNode(struct Snode *sNode, int x, int y) {
 void initSnake(struct Snode *sHead, int length) {
     struct Snode *current = sHead;
 
-    for (int i = 1; i < length + 1; i++) {
+    for (int i = 1; i < length; i++) {
         struct Snode *nextNode = malloc(sizeof(struct Snode));
         initSnakeNode(nextNode, sHead->x, sHead->y + i);
 
@@ -98,6 +124,11 @@ void moveHeadSnake(struct Snode *head, int dx, int dy) {
 
     head->x += dx;
     head->y += dy;
+
+    if (head->x == 0) head->x = 23;
+    else if (head->x == 24) head->x = 1;
+    else if (head->y == 0) head->y = 23;
+    else if (head->y == 24) head->y = 1;
 
     struct Snode *current = head->back;
 
@@ -116,10 +147,10 @@ void moveHeadSnake(struct Snode *head, int dx, int dy) {
 }
 
 void fillArraySnake(struct Snode *startNode, int **arr) {
-    while (startNode->back != NULL) {
-        struct Snode *tempNode = startNode;
-        arr[startNode->y][startNode->x] = 1;
-        startNode = tempNode->back;
+    fillFieldByZero(arr, 25, 25);
+    while (startNode != NULL) {
+        arr[startNode->x][startNode->y] = 1;
+        startNode = startNode->back;
     }
 }
 
@@ -136,4 +167,12 @@ void freeField(int **arr, int height) {
         free(arr[i]);
     }
     free(arr);
+}
+
+void fillFieldByZero(int **field, int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            field[i][j] = 0;
+        }
+    }
 }
